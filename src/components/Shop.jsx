f3e7affe2d36b0c5a0eb6dabad5c13403d74d91b
Alex Duggleby <151508+aduggleby@users.noreply.github.com>
2023-07-18 11:27:54 -0700

    Initial

---
+++ src/components/Shop.jsx
@@ -0,0 +1,14 @@
+'use client'
+
+import Script from "next/script"
+
+export function Shop() {
+
+  return (
+    <>
+<link href="https://embed.minimalwebshop.com/v1.css" rel="stylesheet" />
+    <Script type="module" src="https://embed.minimalwebshop.com/v1.js" />
+<div mws="UGUXCQHK" />
+  </>
+)
+}
