bb8e4753ed1313b11a34929a576d5097b1a25a40
Alex Duggleby <151508+aduggleby@users.noreply.github.com>
2023-07-19 10:29:38 -0700

    Remove CSS

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,7 +14,6 @@ export default function Layout({ children }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <head>
-        <link href="https://embed.minimalwebshop.com/v1.css" rel="stylesheet" />
         <Script type="module" src="https://embed.minimalwebshop.com/v1.js" />
       </head>
       <body className="flex min-h-full flex-col">
